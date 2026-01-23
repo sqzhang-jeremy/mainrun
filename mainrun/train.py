38cc8f90bbf7a120a24f633953f74a2dcabeec81
jeremy <sqzhang.jeremy@gmail.com>
2026-01-23 06:18:48 +0000

    Block and Batch size: update batch_size from 128 to 64

--- mainrun/train.py
+++ mainrun/train.py
@@ -15,7 +15,7 @@ import structlog
 @dataclass
 class Hyperparameters:
     block_size: int = 64
-    batch_size: int = 128
+    batch_size: int = 64
     vocab_size: int = 16_000
     n_layer: int = 6
     n_head: int = 8
