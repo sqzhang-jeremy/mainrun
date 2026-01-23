d5f9f5f30804f3f14406139eeeb08c94dc655eb4
jeremy <sqzhang.jeremy@gmail.com>
2026-01-23 06:09:02 +0000

    Dropout: tune dropput to 0

--- mainrun/train.py
+++ mainrun/train.py
@@ -20,7 +20,7 @@ class Hyperparameters:
     n_layer: int = 6
     n_head: int = 8
     d_model: int = 512
-    dropout: float = 0.05
+    dropout: float = 0
     lr: float = 3e-4 #new Solution2 (was 6e-3)
     weight_decay: float = 0.1
     evals_per_epoch: int = 3
